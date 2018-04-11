eb745c096de6f1efd3d40e94f743dff7a7c7e2cd
Ali At-Tayeb <30572881+attayeba@users.noreply.github.com>
2018-04-11 13:44:36 -0400

    Analysis

--- Unique/Unique.py
+++ Unique/Unique.py
@@ -1,19 +1,25 @@
+from scipy import stats
+import scipy
 import pandas as pd
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv('ReducedData.csv')
+data = pd.read_csv('formated.csv')
+DataPrice = data[['price']]
+OutlierData = data[(np.abs(scipy.stats.zscore(DataPrice)) < 3).all(axis=1)]
 
 # Extract columns that seem to have insight - 48852 x 15
-ReducedData = data[['neighbourhood_group_cleansed','property_type','room_type','accommodates','bathrooms','bedrooms','beds','bed_type','price']]
 
-Neigh = ReducedData['neighbourhood_group_cleansed'] == 'Brooklyn'
-room = ReducedData['room_type'] == 'Private room'
-acco = ReducedData['accommodates'] == 1
-bath = ReducedData['bathrooms'] == 1
-beds = ReducedData['bedrooms'] == 1
+Neigh = OutlierData['neighbourhoodGroup'] == 'Queens'
+room = OutlierData['roomType'] == 'Private room'
+acco = OutlierData['accommodates'] == 1
+bath = OutlierData['bathrooms'] == 1
+beds = OutlierData['bedrooms'] == 1
+city = OutlierData['neighbourhood'] == 'Astoria'
+type = OutlierData['propertyType'] == 'Apartment'
+bed = OutlierData['bedType'] == 'Real Bed'
+ava = OutlierData['availability'] > 0
 
-RD = ReducedData[Neigh & room & acco & bath & beds]
 
-RD.to_csv('unique1.csv')
+RD = OutlierData[Neigh & city & room & acco & bath & beds & type & bed & ava]
+
+RD.to_csv('Queens.csv')
